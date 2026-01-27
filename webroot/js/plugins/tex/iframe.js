73308104482f691fb2739eab309acc4f6bb5d30d
s-nakajima <nakajimashouhei@gmail.com>
2026-01-27 16:00:53 +0900

    fix: プレビューに対して修正漏れ https://github.com/researchmap/RmNetCommons3/issues/3053#issuecomment-3795807293

--- webroot/js/plugins/tex/iframe.js
+++ webroot/js/plugins/tex/iframe.js
@@ -2,19 +2,19 @@
  * tex.iframe.js
  * iframe内でのtexを使うためのscript
  */
-var TEX_IFR = new (function TEX_IFR() {
-  var self = this;
-  var $IFRAME;
-
-  var __setMathJax = function(id) {
-    MathJax.typesetPromise();
-  };
-  self.init = function($ifr) {
-    $IFRAME = $ifr;
-    // init MathJax
-    MathJax = {
-      tex: { inlineMath: [['$$', '$$'], ['\\(', '\\)']] }
-    };
-    __setMathJax();
-  };
-})();
+//var TEX_IFR = new (function TEX_IFR() {
+//  var self = this;
+//  var $IFRAME;
+//
+//  var __setMathJax = function(id) {
+//    MathJax.typesetPromise();
+//  };
+//  self.init = function($ifr) {
+//    $IFRAME = $ifr;
+//    // init MathJax
+//    MathJax = {
+//      tex: { inlineMath: [['$$', '$$'], ['\\(', '\\)']] }
+//    };
+//    __setMathJax();
+//  };
+//})();
